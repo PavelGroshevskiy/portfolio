6d86c8f2783dba97050cf10c1f9642cbccdaff56
Pavel Groshevskiy <pavel.groshevskiy@gmail.com>
2022-12-07 22:43:34 +0700

    fix serv

--- pages/mern-blog.jsx
+++ pages/mern-blog.jsx
@@ -44,11 +44,7 @@ const mernBlog = () => {
 					>
 						<button className="px-8 py-2 mt-4 mr-8">Code</button>
 					</a>
-					<a
-						href="https://blog-mern-front-omega.vercel.app/"
-						target="_blank"
-						rel="noreferrer"
-					>
+					<a href="https://mern-blog-jv6f.onrender.com/" target="_blank" rel="noreferrer">
 						<button className="px-8 py-2 mt-4">Demo</button>
 					</a>
 				</div>
